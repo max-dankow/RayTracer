4726776a2069175a78ff76f8b8028134065214bb
Maxim Dankovtsev <max.dankow@gmail.com>
2016-04-03 02:23:26 +0300

    Correct CmakeList. Show window with Cairo

--- main.cpp
+++ main.cpp
@@ -1,27 +1,53 @@
-//#include <memory>
-//#include "Painter/CairoPainter.h"
+
+#include <gtk/gtk.h>
 #include <cairo/cairo.h>
-#include <iostream>
 
-int main() {
-    cairo_surface_t *surface;
-    cairo_t *cr;
-    std::cout << "Test\n";
-    surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 390, 60);
-    cr = cairo_create(surface);
+static void do_drawing(cairo_t *);
 
+static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr,
+                              gpointer user_data)
+{
+    do_drawing(cr);
+
+    return FALSE;
+}
+
+static void do_drawing(cairo_t *cr)
+{
     cairo_set_source_rgb(cr, 0, 0, 0);
     cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
                            CAIRO_FONT_WEIGHT_NORMAL);
     cairo_set_font_size(cr, 40.0);
 
     cairo_move_to(cr, 10.0, 50.0);
-    cairo_show_text(cr, "CAIRO TASCHIT");
+    cairo_show_text(cr, "Hello, Render");
+}
 
-    cairo_surface_write_to_png(surface, "image.png");
 
-    cairo_destroy(cr);
-    cairo_surface_destroy(surface);
+int main(int argc, char *argv[])
+{
+    GtkWidget *window;
+    GtkWidget *darea;
+
+    gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+    darea = gtk_drawing_area_new();
+    gtk_container_add(GTK_CONTAINER(window), darea);
+
+    g_signal_connect(G_OBJECT(darea), "draw",
+                     G_CALLBACK(on_draw_event), NULL);
+    g_signal_connect(window, "destroy",
+                     G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 90);
+    gtk_window_set_title(GTK_WINDOW(window), "GTK window");
+
+    gtk_widget_show_all(window);
+
+    gtk_main();
 
     return 0;
 }
